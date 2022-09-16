3397039e9f5255ea1dfdcdcb74eda72a1cfa2ad5
polovynkok <polovynkok@gmail.com>
2022-09-16 20:44:27 +0300

    Initial commit

---
+++ int.py
@@ -0,0 +1,11 @@
+from tkinter import N
+
+
+#print("privet makaka :)")
+# короче, я гений!!!
+#print("mne len schitat eto: ", -2, "+", -2, "=", 4, "", sep="|", end="",)
+#print("agusha")
+#print("ahahhaah")
+#print("D \nI \nE")
+print("2 + 2", "=", 2 + 1)
+#print(max(3, 5, 8, 13))
---
+++ tt.py
@@ -0,0 +1,6 @@
+p1 = int(input("Введите число: "))
+
+if p1 > 0:
+    print("Nice!!!")
+if p1 <= 0:
+    print("Nope, try again!")
